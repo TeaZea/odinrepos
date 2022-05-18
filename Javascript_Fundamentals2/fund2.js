c196b7e04a77bc30211e670d50bdd97af8ce6723
Stefano <sena.stefano@gmail.com>
2022-05-18 10:58:11 -0400

    bloody waste of time

--- Javascript_Fundamentals2/fund2.js
+++ Javascript_Fundamentals2/fund2.js
@@ -27,9 +27,42 @@ console.log(dbl);
 //const badQuotes = 'What on Earth?!";
 
 
-//string concatination//
+//string concatination... not working for some reason..//
+//const name = "Chris";
+//const greeting = "Hello, ${name}";
+//console.log(greeting);
+
+//const one = "Hello,";
+//const two = "how are you?";
+//const joined = "${one}${two}";
+//console.log(joined)
+
+
+//in contexqt but doesn't work...//
+
+//const button = document.querySelector('button');
+
+//function greet() {
+  //const name = prompt('What is your name?');
+  //alert(`Hello ${name}, nice to see you!`);
+//}
+
+//button.addEventListener('click', greet);
+
+
+//Concatenation using "+"
+
+const greeting = "Hello";
 const name = "Chris";
-const greeting = "Hello, ${name}";
-console.log(greeting);
+console.log(greeting + ", " + name);
+
+
+//just gonna stop doing these exercises as they are just reusing constants and I don't want to keep on redoing .js and index files.
+//also ${} isn't concatinating.
+
+
+
+
+
 
 
