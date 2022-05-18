9fcda14e9f0f62a98e2d4a308a8fce5d7ce43299
Stefano <sena.stefano@gmail.com>
2022-05-18 10:44:02 -0400

    Basic code-along lessons on strings.

--- Javascript_Fundamentals2/fund2.js
+++ Javascript_Fundamentals2/fund2.js
@@ -1,2 +1,35 @@
 //test//
 
+//creating a string//
+
+const string = 'The revolution will not be televised.';
+console.log(string);
+
+//missing quote errors//
+
+//const badString1 = This is a test;
+//const badString2 = 'This is a test;
+//const badString3 = This is a test';
+
+//CORRECT//
+
+const badString = string;
+console.log(badString);
+
+//single and double quotes//
+
+const sgl = 'Single Quotes.';
+const dbl = "Double Quotes.";
+console.log(sgl);
+console.log(dbl);
+
+//INCORECT//
+//const badQuotes = 'What on Earth?!";
+
+
+//string concatination//
+const name = "Chris";
+const greeting = "Hello, ${name}";
+console.log(greeting);
+
+
